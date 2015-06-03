2f1598ec32edcf5776718aba2827848e110b3b20
Alvaro Costa Neto <nepheus.br@gmail.com>
2015-06-03 20:11:45 -0300

    Adicionei o código do analisador léxico para números naturais.

--- lexi.c
+++ lexi.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#define true 1
+#define false 0
+typedef int bool;
 
 // Para todas as regras abaixo:
 // dígito = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
@@ -16,12 +21,49 @@
 // T = {dígito}
 // R:
 //   1) S -> dígito S | dígito
-void lexi_natural_ebnf(char *entrada)
+bool lexi_natural_ebnf(char *entrada)
 {
+  unsigned int indice = 0;
+  // Verifica se o primeiro caractere é um dígito (obrigatório)
+  if (!isdigit(entrada[indice]))
+    return false;
+  indice++;
+  // Verifica os demais caracteres que houverem, até encontrar o final da cadeia ou um caractere inválido
+  while (entrada[indice] != '\0')
+  {
+    if (!isdigit(entrada[indice]))
+      return false;
+    indice++;
+  }
+  return true;
 }
 
-void lexi_natural_automato(char *entrada)
+bool lexi_natural_automato(char *entrada)
 {
+  unsigned int indice = 0;
+  unsigned int state = 0;
+  switch (state)
+  {
+    case 0:
+      // Transição do estado 0 para o estado 1, lendo dígito
+      if (isdigit(entrada[indice]))
+        state = 1;
+      else
+        return false; // Travamento ou estado final inválido!
+      break;
+    case 1:
+      // Laço no estado 1, lendo dígito
+      if (isdigit(entrada[indice]))
+        state = 1;
+      else
+        // Fim da análise
+        if (entrada[indice] == '\0')
+          return true;
+        else
+          return false; // Travamento!
+       break;
+  }
+  return true;
 }
 
 int main(int argc, char **argv)
@@ -29,7 +71,10 @@ int main(int argc, char **argv)
   char entrada[51];
   scanf(" %s", entrada);
   // A função equivalente ao analisador léxico deve ser executada abaixo
-  lexi_inteiro_ebnf(entrada);
+  if (lexi_natural_ebnf(entrada))
+    printf("Elemento léxico aceito.\n");
+  else
+    printf("Houve um erro de execução: o elemento léxico especificado não foi encontrado.\n");
   return 0;
 }
 
