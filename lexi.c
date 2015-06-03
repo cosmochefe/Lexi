ef76230d2ae0ef77a5e0803e7668fb5f762c5545
Alvaro Costa Neto <nepheus.br@gmail.com>
2015-06-03 20:19:39 -0300

    Modifiquei o código da versão EBNF do analisador de números naturais para ficar mais coerente com a EBNF em si e adicionei o laço que estava faltando no analisador por autômato finito.

--- lexi.c
+++ lexi.c
@@ -28,13 +28,11 @@ bool lexi_natural_ebnf(char *entrada)
   if (!isdigit(entrada[indice]))
     return false;
   indice++;
-  // Verifica os demais caracteres que houverem, até encontrar o final da cadeia ou um caractere inválido
-  while (entrada[indice] != '\0')
-  {
-    if (!isdigit(entrada[indice]))
-      return false;
+  // Verifica os demais caracteres que houverem, até encontrar um caractere que não for dígito
+  while (isdigit(entrada[indice]))
     indice++;
-  }
+  if (entrada[indice] != '\0')
+    return false;
   return true;
 }
 
@@ -42,26 +40,30 @@ bool lexi_natural_automato(char *entrada)
 {
   unsigned int indice = 0;
   unsigned int state = 0;
-  switch (state)
+  while (1)
   {
-    case 0:
-      // Transição do estado 0 para o estado 1, lendo dígito
-      if (isdigit(entrada[indice]))
-        state = 1;
-      else
-        return false; // Travamento ou estado final inválido!
-      break;
-    case 1:
-      // Laço no estado 1, lendo dígito
-      if (isdigit(entrada[indice]))
-        state = 1;
-      else
-        // Fim da análise
-        if (entrada[indice] == '\0')
-          return true;
+    switch (state)
+    {
+      case 0:
+        // Transição do estado 0 para o estado 1, lendo dígito
+        if (isdigit(entrada[indice]))
+          state = 1;
         else
-          return false; // Travamento!
-       break;
+          return false; // Travamento ou estado final inválido!
+        break;
+      case 1:
+        // Laço no estado 1, lendo dígito
+        if (isdigit(entrada[indice]))
+          state = 1;
+        else
+          // Fim da análise
+          if (entrada[indice] == '\0')
+            return true;
+          else
+            return false; // Travamento!
+         break;
+    }
+    indice++;
   }
   return true;
 }
@@ -71,7 +73,7 @@ int main(int argc, char **argv)
   char entrada[51];
   scanf(" %s", entrada);
   // A função equivalente ao analisador léxico deve ser executada abaixo
-  if (lexi_natural_ebnf(entrada))
+  if (lexi_natural_automato(entrada))
     printf("Elemento léxico aceito.\n");
   else
     printf("Houve um erro de execução: o elemento léxico especificado não foi encontrado.\n");
